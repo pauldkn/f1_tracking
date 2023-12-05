22068b74546507b0d3b12022afa80a29b71dacf7
Paul Dkn <pauldkn.code@gmail.com>
2023-12-05 18:12:10 +0100

    remove logs & useless imports

--- components/LeagueResultSection.tsx
+++ components/LeagueResultSection.tsx
@@ -1,18 +1,8 @@
-import { useEffect } from 'react'
-import { getPiloteLeagueResults, getTeamLeagueResults } from '@/utils'
 import { LeagueResultPiloteTable } from './LeagueResultPiloteTable'
 import { LeagueResultTeamTable } from './LeagueResultTeamTable'
 import { Title } from './ui/Title'
 
 export const LeagueResultSection = () => {
-  const leagueResults = getPiloteLeagueResults()
-  const teamResults = getTeamLeagueResults()
-
-  useEffect(() => {
-    console.log('🚀 ~ LeagueResultSection ~ leagueResults:', leagueResults)
-    console.log('🚀 ~ LeagueResultSection ~ teamResults:', teamResults)
-  }, [])
-
   return (
     <section className="flex flex-wrap justify-center gap-10">
       <div className="flex flex-col items-center gap-5">
